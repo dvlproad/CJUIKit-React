ea0c839476842ecd7b1b80b18818506fb52921c8
dvlproad <dvlproad@163.com>
2020-04-28 14:06:46 +0800

    【Feature】自定义主题 1、yarn add less less-loader 2、修改 config-overrides.js 3、额外增加 yarn add style-loader css-loader

--- cjuikit-antd-mobile-demo/src/App.js
+++ cjuikit-antd-mobile-demo/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Button } from 'antd-mobile';
+import { Button, SearchBar} from 'antd-mobile';
 import './App.css';
 
 const App = () => (
     <div className="App">
-      <Button type="primary">Button</Button>
+        <Button type="primary">Button</Button>
+        <SearchBar placeholder="Search" maxLength={8} />
     </div>
 );
 
